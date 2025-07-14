0bee6d196d687c1ca86c0b9de9e3379fb619ebc9
Reeco <rehans.axis@gmail.com>
2025-07-14 07:54:57 +0000

    the screen is now stuck not let overflow y out the page contain it in so

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -106,7 +106,7 @@ function MainLayoutContent({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="grid h-screen w-full overflow-hidden md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -241,7 +241,7 @@ function MainLayoutContent({ children }: { children: React.ReactNode }) {
           </div>
           <UserNav />
         </header>
-        <main className="flex flex-1 flex-col gap-4 overflow-auto p-4 lg:p-6">
+        <main className="flex-1 overflow-auto p-4 lg:p-6">
           {children}
         </main>
       </div>
